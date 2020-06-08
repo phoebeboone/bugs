2225724e70574b9c4b6ed34763325f10e08110b8
Phoebe <45608057+phoebeboone@users.noreply.github.com>
2020-06-07 19:22:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,245 @@
+$(
+  function(){
+
+  $(".fly").hover (
+    function(){
+      $(".fly").hide();
+      $(".fly").delay(2000).queue(
+        function(next) {
+          $(".fly").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".centipede").hover (
+    function(){
+      $(".centipede").hide();
+      $(".centipede").delay(5000).queue(
+        function(next) {
+          $(".centipede").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".spider").hover (
+    function(){
+      $(".spider").hide();
+      $(".spider").delay(3000).queue(
+        function(next) {
+          $(".spider").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".centipede_2").hover (
+    function(){
+      $(".centipede_2").hide();
+      $(".centipede_2").delay(6000).queue(
+        function(next) {
+          $(".centipede_2").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".spider_2").hover (
+    function(){
+      $(".spider_2").hide();
+      $(".spider_2").delay(1000).queue(
+        function(next) {
+          $(".spider_2").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".beetle").hover (
+    function(){
+      $(".beetle").hide();
+      $(".beetle").delay(1500).queue(
+        function(next) {
+          $(".beetle").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".spider_3").hover (
+    function(){
+      $(".spider_3").hide();
+      $(".spider_3").delay(2030).queue(
+        function(next) {
+          $(".spider_3").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".spider_4").hover (
+    function(){
+      $(".spider_4").hide();
+      $(".spider_4").delay(3000).queue(
+        function(next) {
+          $(".spider_4").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".roach").hover (
+    function(){
+      $(".roach").hide();
+      $(".roach").delay(1000).queue(
+        function(next) {
+          $(".roach").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".centipede_3").hover (
+    function(){
+      $(".centipede_3").hide();
+      $(".centipede_3").delay(4000).queue(
+        function(next) {
+          $(".centipede_3").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".roach_2").hover (
+    function(){
+      $(".roach_2").hide();
+      $(".roach_2").delay(2000).queue(
+        function(next) {
+          $(".roach_2").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".bug").hover (
+    function(){
+      $(".bug").hide();
+      $(".bug").delay(5000).queue(
+        function(next) {
+          $(".bug").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".bug_2").hover (
+    function(){
+      $(".bug_2").hide();
+      $(".bug_2").delay(6000).queue(
+        function(next) {
+          $(".bug_2").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".spider_5").hover (
+    function(){
+      $(".spider_5").hide();
+      $(".spider_5").delay(2000).queue(
+        function(next) {
+          $(".spider_5").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".spider_6").hover (
+    function(){
+      $(".spider_6").hide();
+      $(".spider_6").delay(2000).queue(
+        function(next) {
+          $(".spider_6").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".bee").hover (
+    function(){
+      $(".bee").hide();
+      $(".bee").delay(2000).queue(
+        function(next) {
+          $(".bee").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".bug_3").hover (
+    function(){
+      $(".bug_3").hide();
+      $(".bug_3").delay(2000).queue(
+        function(next) {
+          $(".bug_3").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".bee_2").hover (
+    function(){
+      $(".bee_2").hide();
+      $(".bee_2").delay(7000).queue(
+        function(next) {
+          $(".bee_2").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".bee_3").hover (
+    function(){
+      $(".bee_3").hide();
+      $(".bee_3").delay(1500).queue(
+        function(next) {
+          $(".bee_3").show();
+          next();
+        }
+      )
+    }
+  )
+
+  $(".spider_7").hover (
+    function(){
+      $(".spider_7").hide();
+      $(".spider_7").delay(2000).queue(
+        function(next) {
+          $(".spider_7").show();
+          next();
+        }
+      )
+    }
+  )
+
+}
+)
